b99127900f857040b0f79dd9d200ee05d94acd1a
Sandro <sandro.gantze@icloud.com>
2026-02-15 16:50:04 +0100

    comment

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -576,7 +576,8 @@ const Index = () => {
 
         {filteredItems.length === 0 ? (
           <div className="flex flex-col items-center justify-center py-20 text-center">
-            {/* ... rest of your empty state f... */}
+            {/* ... rest of your empty state fPERFECTEBAY7
+... */}
           </div>
         ) : (
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
